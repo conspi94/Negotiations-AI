a3c34f2474380114633d95b99cb1723498480279
conspi94 <126285448+conspi94@users.noreply.github.com>
2025-05-02 22:35:14 -0700

    Add files via upload

---
+++ routes/ai.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const router = express.Router();
+const { generateNegotiation } = require('../controllers/aiController');
+router.post('/negotiate', generateNegotiation);
+module.exports = router;
