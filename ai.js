aa87cb760b847abd8b647d53f9a36adccc13e82a
conspi94 <126285448+conspi94@users.noreply.github.com>
2025-05-02 22:21:15 -0700

    Delete ai.js

--- ai.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { generateNegotiation } = require("../controllers/aiController");
-
-router.post("/negotiate", generateNegotiation);
-
-module.exports = router;
