d6c0d6e817e99a343f7170ae6a1c20273ffa761a
conspi94 <126285448+conspi94@users.noreply.github.com>
2025-05-02 22:40:53 -0700

    Delete server.js

--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-require('dotenv').config();
-app.use(express.json());
-const aiRoutes = require('./routes/ai');
-app.use('/api/ai', aiRoutes);
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
